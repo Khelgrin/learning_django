504f85699693d4795843b6bcdf615491f3624cbb
khel <khelgrin@gmail.com>
2019-09-08 15:10:34 +0200

    lesson 3

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+# it is super important that the names in the urlpatterns
+# are unique, since we are using them to route to the
+# specific locations on our page
+
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+
+posts = [
+    {
+        'author': 'Khel',
+        'title': 'title1',
+        'content': 'first post',
+        'date': '21.10.2018'
+    },
+    {
+        'author': 'Khel2',
+        'title': 'title2',
+        'content': 'second post',
+        'date': '21.10.2019'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    """The render function from django.shotrcuts
+       is a shortcut for:
+    - loading the template in,
+    - render it,
+    - pass that to the HTTP response"""
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title':'About'})
